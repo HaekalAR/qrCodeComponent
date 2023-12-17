c56df2c1140b2bc85646c44704ab8424a44ba568
Haekal Abdillah Ramadhan <ekal26b@gmail.com>
2023-12-17 13:25:06 +0700

    A little bit of changes in card

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,12 +1,12 @@
 const Card = () => {
     return (
         <div className="h-screen w-full bg-[--light-gray] p-4 flex items-center justify-center">
-            <div className="flex flex-col w-[17.5em] h-fit p-4 bg-[--white] rounded-xl gap-5">
+            <div className="flex flex-col md:w-[17.5em] w-[16em] h-fit p-4 bg-[--white] rounded-xl gap-5 shadow-[--grayish-blue]/90 shadow-md">
                 <div>
                     <img src="/image-qr-code.png" className="rounded-lg" />
                 </div>
                 <div className="text-center px-2">
-                    <h3 className="font-[700] text-lg pb-2 text-[--dark-blue]">
+                    <h3 className="font-[700] md:text-lg text-base pb-2 text-[--dark-blue]">
                         Improve your front-end skills by building projects
                     </h3>
                     <p className="text-sm pb-4 text-[--grayish-blue]">
